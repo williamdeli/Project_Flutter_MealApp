c3850cbf3b02d93fe25fa83f67076d856d7be752
Wiedjaja Atmadja <swiedjaja@binus.ac.id>
2023-04-06 15:27:15 +0700

    Step 4. end of session 6 part 1

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'recipe_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final String id;
@@ -7,19 +8,36 @@ class CategoryItem extends StatelessWidget {
 
   const CategoryItem(this.id, this.title, this.color, {super.key});
 
+  void selectCategory(BuildContext context) {
+    // Navigator.of(context).pushNamed(RecipeScreen.routeName, arguments: {
+    //   'id': id,
+    //   'title': title,
+    // });
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (_) {
+        return RecipeScreen(categoryId: id, categoryTitle: title);
+      },
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [color.withOpacity(0.7), color],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [color.withOpacity(0.7), color],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(15),
         ),
-        borderRadius: BorderRadius.circular(15),
+        child: Text(title, style: Theme.of(context).textTheme.titleMedium),
       ),
-      child: Text(title, style: Theme.of(context).textTheme.titleMedium),
     );
   }
 }
